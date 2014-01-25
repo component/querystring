ce984df47cac2a65bebffc266fc2a36088a900d3
Christopher Rogers <chrissrogers@gmail.com>
2014-01-24 20:43:31 -0800

    Adds the ability to parse complex querystrings of arrays and nested objects
    Signed-off-by: Christopher Rogers <chrissrogers@gmail.com>

--- index.js
+++ index.js
@@ -16,17 +16,68 @@ var trim = require('trim');
 exports.parse = function(str){
   if ('string' != typeof str) return {};
 
-  str = trim(str);
+  str = trim(str).replace(/^\?/, '');
   if ('' == str) return {};
-  if ('?' == str.charAt(0)) str = str.slice(1);
 
   var obj = {};
   var pairs = str.split('&');
   for (var i = 0; i < pairs.length; i++) {
     var parts = pairs[i].split('=');
-    obj[parts[0]] = null == parts[1]
-      ? ''
-      : decodeURIComponent(parts[1]);
+    var key = decodeURIComponent(parts[0]);
+    var keys = key.split('][');
+    var lastKeyPos = keys.length - 1;
+
+    // splits initial 'foo[bar' and removes terminal ']'
+    if (~keys[0].indexOf('[') && keys[lastKeyPos].slice(-1) == ']') {
+      keys[lastKeyPos] = keys[lastKeyPos].slice(0, -1);
+      keys = keys.shift().split('[').concat(keys);
+      lastKeyPos = keys.length - 1;
+    }
+
+    var val = decodeURIComponent(parts[1] || '');
+
+    if (lastKeyPos) {
+
+      /**
+       * complex nesting
+       *
+       * cur starts at obj root
+       * '' indicates [], aka array push. key becomes array length
+       * if last key, set val and finish
+       * if level exists, move into it
+       *   otherwise look to the next key to determine if this new level
+       *   should be an object ([string]) or array ([], [number])
+       */
+
+      var cur = obj;
+      for (var j = 0; j <= lastKeyPos; j++) {
+        key = keys[j] === '' ? cur.length : keys[j];
+        if (j === lastKeyPos) {
+          cur[key] = val;
+        } else if (cur[key]) {
+          cur = cur[key];
+        } else {
+          cur = cur[key] = keys[j + 1] && isNaN(keys[j + 1]) ? {} : [];
+        }
+      }
+    } else {
+
+      /**
+       * plain key=value
+       *
+       * if key is an array, push value
+       * if key already exists, create an array with old and new value
+       * otherwise just set key=value
+       */
+
+      if (obj[key] && obj[key].push) {
+        obj[key].push(val);
+      } else if (obj[key] !== undefined) {
+        obj[key] = [obj[key], val];
+      } else {
+        obj[key] = val;
+      }
+    }
   }
 
   return obj;
--- test/parse.js
+++ test/parse.js
@@ -36,4 +36,46 @@ describe('.parse(str)', function(){
       expect(obj).to.eql({ name: 'tobi', species: 'ferret' });
     })
   })
+
+  describe('when arrays are implied', function() {
+    it('should parse key[]=value as an array', function() {
+      var obj = query.parse('names[]=tobi&names[]=toby&names[]=tobias');
+      expect(obj).to.eql({ names: ['tobi', 'toby', 'tobias'] });
+    })
+
+    it('should parse key=value1&key=value2 as an array', function() {
+      var obj = query.parse('names=tobi&names=toby&names=tobias');
+      expect(obj).to.eql({ names: ['tobi', 'toby', 'tobias'] });
+    })
+
+    it('should parse key[n]=value as an array', function() {
+      var obj = query.parse('names[]=tobi&names[10]=toby');
+      expect(Array.isArray(obj.names)).to.be(true);
+      expect(obj.names[0]).to.eql('tobi');
+      expect(obj.names[10]).to.eql('toby');
+      expect(obj.names.length).to.eql(11);
+    });
+  })
+
+  describe('when nested values are passed', function() {
+    it('should merge nested values into a tree', function() {
+      var obj = query.parse('name[first]=tobi&name[last]=seaworth&species=ferret');
+      expect(obj).to.eql({ name: { first: 'tobi', last: 'seaworth' }, species: 'ferret' });
+    })
+
+    it('should parse deeply nested values', function() {
+      var obj = query.parse('i[am][a][very][deeply][nested]=value');
+      expect(obj).to.eql({ i: { am: { a: { very: { deeply: { nested: 'value' } } } } } });
+    })
+
+    it('should parse complex trees', function() {
+      var obj = query.parse('people[tobi][boring]=false&people[tobi][hobbies][]=birdwatching&people[tobi][hobbies][]=archery&people[toby][boring]=true&people[toby][hobbies][]');
+      expect(obj).to.eql({
+        people: {
+          tobi: { boring: 'false', hobbies: ['birdwatching', 'archery'] },
+          toby: { boring: 'true', hobbies: [''] }
+        }
+      });
+    })
+  })
 })
